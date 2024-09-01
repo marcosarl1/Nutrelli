050f26748934c585ec18f7141c6e3a199cc122da
Marcos Júnior <marcosajr11@gmail.com>
2024-09-01 16:08:47 -0300

    refactor: Change on getProdtuo() method

--- src/main/java/com/nutrelli/dao/ProdutoDAO.java
+++ src/main/java/com/nutrelli/dao/ProdutoDAO.java
@@ -3,7 +3,7 @@ package com.nutrelli.dao;
 import com.nutrelli.model.Produto;
 import com.nutrelli.util.JPAUtil;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 
 import java.util.List;
 
@@ -52,8 +52,8 @@ public class ProdutoDAO {
     public List<Produto> getProduto(String filter) {
         EntityManager entityManager = JPAUtil.getEntityManager();
         try {
-            Query query = entityManager.createQuery(
-                    "SELECT p from Produto p WHERE (:filter IS NULL OR p.nome LIKE :filter OR p.categoria.nome LIKE :filter)");
+            TypedQuery<Produto> query = entityManager.createQuery(
+                    "SELECT p from Produto p WHERE (:filter IS NULL OR p.nome LIKE :filter OR p.categoria.nome LIKE :filter)", Produto.class);
             query.setParameter("filter", filter.isEmpty() ? null : "%" + filter + "%");
             return query.getResultList();
         } catch (Exception e) {
